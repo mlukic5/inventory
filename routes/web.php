da4b2ee954451cd7a10c612e9884c29d3d792f09
Manuel Lukic <mlukic@schueler.hakzell.at>
2026-01-19 12:14:18 +0100

    init commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+Route::get('/',function(){
+    return "Hallo, Welt!";
+});
+
+Route::get('/about', function() {
+    return "<html><title>About</title><body><h1>About me</h1></body></html>";
+});
+
+Route::get('/me', function() {
+    return view('test');
+});
+
+Route::get('/impressum', function(){
+    return view('impressum'); //impressum.blade.php wird verwendet (im views-Ordner)
+});
